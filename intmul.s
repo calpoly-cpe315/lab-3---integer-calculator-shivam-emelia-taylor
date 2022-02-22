dfcea8f9420cc06160701fb2b7b3e612e408c38b
Taylor Bedrosian <taylorbedrosian@MacBook-Pro.local>
2022-02-21 21:19:09 -0800

    sub and mult

--- intmul.s
+++ intmul.s
@@ -4,8 +4,39 @@
     .global intmul
 
 intmul:
-	stp   x29, x30, [sp, -16]!
-	add   x29, sp, 0
+   /* x0 = num 1 and return location, x2 = num2
+   /* x3 = a, x4 =b, x1=c, x5 = additionC x6=b and result, x7 is temp b
+	/*and - carry*/
+	stp    x29, x30, [sp, -16]!
 
-	ldp   x29, x30, [sp], 16
+   mov   x3, x0
+   mov   x4, x2
+   mov   x1, #0
+multloop:
+   and   x6, x4, #1
+   cmp   x6, #0
+   b.eq  cont
+   mov   x7, x1
+   mov   x2, x3
+
+multaddloop: 
+   and   x5, x7, x2
+   eor   x7, x7, x2
+   lsl   x2, x5, #1
+
+   cmp   x2, #0
+   b.ne  multaddloop
+   mov   x1, x7
+
+cont:
+   lsl   x3, x3, #1
+   lsr   x4, x4, #1   
+   
+   cmp   x4, #0
+   b.eq  multloop
+
+   mov   x0, x1
+
+	ldp    x29, x30, [sp], 16
 	ret
+   
--- intsub.s
+++ intsub.s
@@ -4,9 +4,26 @@
     .global intsub
 
 intsub:
+   /* x0 = num 1 and return location, x2 = num2
+   /* x3 = x, x4 =y, x5=borrow, x6=notX
 	/*negate x2 and x1, x2 carry*/
 	stp    x29, x30, [sp, -16]!
-	add    x29, sp, 0
+	
+   mov   x3, x0
+   mov   x4, x2
+
+subloop: 
+   
+   mvn   x6, x3
+   and   x5, x6, x4
+   eor   x3, x3, x4
+   lsl   x4, x5, #1
+
+   cmp   x4, #0
+   b.ne  subloop
+
+   mov   x0, x3
+
 
 	ldp    x29, x30, [sp], 16
 	ret
